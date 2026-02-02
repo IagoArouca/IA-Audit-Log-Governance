284d43348f6d758b1b606a3dd240255b4bd5b1d5
Iago <iagoaroucaferreira@hotmail.com>
2026-02-02 14:53:51 -0300

    feat: I created the JwtStrategy

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,7 +3,15 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'sentinel-secret-key-2026',
+      signOptions: { expiresIn: '1d' }, // Token vale por 1 dia
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, PrismaService], // Adicione o JwtStrategy aqui
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
