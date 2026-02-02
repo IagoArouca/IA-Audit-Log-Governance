b46d9c733db867fceae8cdc94d6a8807b9de12c4
Iago <iagoaroucaferreira@hotmail.com>
2026-02-02 14:45:00 -0300

    fix: I adjusted the audit-logs.controller.ts

--- backend/src/audit-logs/audit-logs.controller.ts
+++ backend/src/audit-logs/audit-logs.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { AuditLogsService } from './audit-logs.service';
+import { JwtAuthGuard} from '../auth/guards/jwt-auth.guard';
+
 
 @Controller('audit-logs')
-export class AuditLogsController {}
+export class AuditLogsController {
+    constructor(private auditLogService: AuditLogsService){}
+
+    @UseGuards(JwtAuthGuard)
+    @Get('stats')
+    async getDashboardStats(@Request() req) {
+        return this.auditLogService.getStats(req.user.sub);
+    }
+}
