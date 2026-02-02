1055ba63800a4b8d3e1859566d89ec9f42f7baab
Iago <iagoaroucaferreira@hotmail.com>
2026-02-02 16:20:19 -0300

    feat: I created the Apikey service

---
+++ backend/src/api-key/api-key.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { randomBytes } from 'crypto';
+
+@Injectable()
+export class ApiKeyService {
+    constructor(private prisma: PrismaService){}
+
+    async create(userId: string, name: string) {
+        const randomString = randomBytes(24).toString('hex');
+
+        const apiKey = `IA_audit_${randomString}`
+
+        return this.prisma.apiKey.create({
+            data: {
+                key: apiKey,
+                name: name, 
+                userId: userId,
+            },
+            select: {
+                id: true,
+                key: true,
+                name: true,
+                createdAt: true,
+            }
+        });
+    }
+
+    async findAll(userId: string) {
+        return this.prisma.apiKey.findMany({
+            where: { userId },
+            orderBy: { createdAt: 'desc' },
+        });
+    }
+    
+    async revoke(id:string, userId: string){
+        return this.prisma.apiKey.deleteMany({
+            where: { id, userId },
+        });
+    }
+}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AuditLogsModule } from './audit-logs/audit-logs.module';
+import { ApiKeyModuleModule } from './api-key-module/api-key-module.module';
+import { ApiKeyModule } from './api-key/api-key.module';
 
 @Module({
-  imports: [AuthModule, AuditLogsModule],
+  imports: [AuthModule, AuditLogsModule, ApiKeyModuleModule, ApiKeyModule],
   controllers: [AppController],
   providers: [AppService],
 })
